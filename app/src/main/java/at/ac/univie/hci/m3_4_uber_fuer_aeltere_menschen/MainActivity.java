1c8a88ba90a5b17b7267afc3f747e7018fd7ba9f
thomny <thomas.nguyen.oeffentlich@gmail.com>
2023-05-03 00:46:29 +0200

    Test Commit

---
+++ app/src/main/java/at/ac/univie/hci/m3_4_uber_fuer_aeltere_menschen/MainActivity.java
@@ -0,0 +1,14 @@
+package at.ac.univie.hci.m3_4_uber_fuer_aeltere_menschen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
